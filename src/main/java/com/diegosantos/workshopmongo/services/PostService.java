c1edc54c058b567ba05e4b2b663336ffb017bc19
Diego Santos <diegodev.silva@gmail.com>
2023-11-15 20:41:18 -0300

    obtendo um post por id

---
+++ src/main/java/com/diegosantos/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.diegosantos.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegosantos.workshopmongo.domain.Post;
+import com.diegosantos.workshopmongo.domain.User;
+import com.diegosantos.workshopmongo.repository.PostRepository;
+import com.diegosantos.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service // falar para o spring que essa classe pode ser injetavel em outras classes
+public class PostService {
+	
+	@Autowired // busca automaticamente e instancia a classe que voce chamar abaixo dele
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+	    Optional<Post> obj = repo.findById(id);
+	    return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
