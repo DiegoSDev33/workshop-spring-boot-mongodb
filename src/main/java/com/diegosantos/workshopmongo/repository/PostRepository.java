f2a9015b9e79cacc719b488cf726b6b1a71b9455
Diego Santos <diegodev.silva@gmail.com>
2023-11-15 21:41:05 -0300

    Consulta simples com @Query

--- src/main/java/com/diegosantos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/diegosantos/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.diegosantos.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.diegosantos.workshopmongo.domain.Post;
@@ -11,4 +12,11 @@ import com.diegosantos.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); // ignore case fara com que sera ignorado se as letras sao maiusculas ou minusculas
+
+	@Query("{'title':{ $regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
+
+
+
+
 }
